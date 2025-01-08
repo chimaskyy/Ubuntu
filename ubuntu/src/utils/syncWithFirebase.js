c584d34ae3884fb2f4e3c64f03d6cfe427b03027
chimaskyy <chiamakahj@gmail.com>
2025-01-08 06:28:32 +0100

    del drive file

--- ubuntu/src/utils/syncWithFirebase.js
+++
@@ -1,8 +0,0 @@
-const syncWithFirebase =
-  (reducer, syncAction) => (payload, userId) => (dispatch, getState) => {
-    dispatch(reducer(payload)); // Update the local state
-    const { items } = getState().cart; // Access the updated cart state
-    dispatch(syncAction({ userId, cart: items })); // Sync with Firebase
-  };
-
-export default syncWithFirebase;
